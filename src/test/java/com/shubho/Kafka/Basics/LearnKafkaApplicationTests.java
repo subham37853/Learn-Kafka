90165191c2afdbba0f27d45611cc1531ed8eb0ec
subham37853 <subham.prasad62@gmail.com>
2022-11-18 10:40:34 +0530

    Adding new project for learning kafka

---
+++ src/test/java/com/shubho/Kafka/Basics/LearnKafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shubho.Kafka.Basics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnKafkaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
